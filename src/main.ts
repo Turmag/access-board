7e0143fd978bb083bb3268d4c8f31f537cd9cc56
Pavel Vinogradov <pavel.vinogradov@mpstats.io>
2025-01-25 18:59:42 +0300

    Пересобрана часть проекта с импортами

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,16 @@ import '@/style.scss';
 import '@/assets/scss/notification.scss';
 import 'floating-vue/dist/style.css';
 import 'vue-multiselect/dist/vue-multiselect.css';
+import Notifications from '@kyvg/vue3-notification';
+import { createPinia } from 'pinia';
+import { createApp } from 'vue';
 import App from '@/App.vue';
+import FloatingVue from 'floating-vue';
 
-export { App };
+const pinia = createPinia();
+const app = createApp(App);
+app.use(pinia);
+app.use(Notifications);
+app.use(FloatingVue);
+
+export { app };
