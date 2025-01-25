3d17701caa57758518d292bada3559c94065cb95
Pavel Vinogradov <pavel.vinogradov@mpstats.io>
2025-01-25 21:16:53 +0300

    Восстановлен правильный файл

---
+++ src/shared/composables/useTheme.ts
@@ -0,0 +1,48 @@
+import { useStorage } from '@vueuse/core';
+import {
+    onMounted,
+    ref,
+    watch,
+    watchEffect,
+} from 'vue';
+import type { IMainStore } from '@/shared/types';
+
+export function useTheme(store: IMainStore) {
+    const isDarkMode = useStorage(store.darkModeName, false);
+    const isSavedDarkMode = useStorage(store.savedDarkModeName, false);
+    const matches = ref(true);
+
+    const setDarkMode = () => {
+        document.body.classList.add('dark');
+        isDarkMode.value = true;
+    };
+
+    const setLightMode = () => {
+        document.body.classList.remove('dark');
+        isDarkMode.value = false;
+    };
+
+    watchEffect(onInvalidate => {
+        const media = window.matchMedia('(prefers-color-scheme: dark)');
+        if (media.matches !== matches.value) matches.value = media.matches;
+
+        const onChange = () => matches.value = media.matches;
+        media.addEventListener('change', onChange);
+
+        onInvalidate(() => media.removeEventListener('change', onChange));
+    });
+
+    watch(
+        () => matches.value,
+        value => {
+            if (!isSavedDarkMode.value) value ? setDarkMode() : setLightMode();
+        },
+    );
+
+    onMounted(() => {
+        if (!isSavedDarkMode.value) {
+            isDarkMode.value = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        }
+        if (isDarkMode.value) setDarkMode();
+    });
+}
